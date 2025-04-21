1c620b36e73be4fd3ff93c7ddd8b72187d6c4fc6
ItzGalaxy15 <152269683+ItzGalaxy15@users.noreply.github.com>
2025-04-21 15:45:51 +0200

    added front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,10 @@
-import { useState } from 'react'
-
-function App() {
-
+import React from 'react';
+ 
+export const App = ()=> {
   return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+<div>
+      Hello React!
+</div>
+  );
 }
-
-export default App
+ 

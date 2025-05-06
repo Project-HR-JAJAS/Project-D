ec164752551fbbce3deb1037102ffd30be5752f3
ItzGalaxy15 <152269683+ItzGalaxy15@users.noreply.github.com>
2025-05-06 14:36:28 +0200

    Update program.py

--- backend/program.py
+++ backend/program.py
@@ -442,4 +442,4 @@ if __name__ == "__main__":
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
